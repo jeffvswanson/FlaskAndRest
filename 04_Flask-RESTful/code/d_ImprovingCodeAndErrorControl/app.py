32c88315611feffbd83fe1085a665b08ac2f5f79
jeffvswanson <jeffery.swanson@datarobot.com>
2019-10-15 19:38:44 -0600

    Add filter and error control

---
+++ 04_Flask-RESTful/code/d_ImprovingCodeAndErrorControl/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': None}, 200 if item else 404 # Not found
+
+    def post(self, name):
+        # Error first design
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201 # Created
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
+
+app.run(port=5000, debug=True)
