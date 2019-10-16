67c57b704f9f496c7f18fd6349d0a35dd40c5dd9
jeffvswanson <jeffery.swanson@datarobot.com>
2019-10-15 20:53:16 -0600

    Add DELETE item capability

---
+++ 04_Flask-RESTful/code/f_DELETE/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
+
+app = Flask(__name__)
+app.secret_key = 'jeff'
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+items = []
+
+class Item(Resource):
+    @jwt_required()
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404 # Not found
+
+    def post(self, name):
+        # Error first design
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400 # Bad Request
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201 # Created
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted.'}
+
+class Items(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
+
+app.run(port=5000, debug=True)
