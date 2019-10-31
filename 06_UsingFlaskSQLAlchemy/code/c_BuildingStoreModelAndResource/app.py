6f7e4643ed5dee4d3c401803670ab67507cc58be
jeffvswanson <jeff.v.swanson@gmail.com>
2019-10-31 15:46:02 -0600

    Add Store resource endpoints

--- 06_UsingFlaskSQLAlchemy/code/c_BuildingStoreModelAndResource/app.py
+++ 06_UsingFlaskSQLAlchemy/code/c_BuildingStoreModelAndResource/app.py
@@ -6,6 +6,7 @@ from security import authenticate, identity
 
 from Resources.item import Item, ItemList
 from Resources.user import UserRegister
+from Resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -21,6 +22,8 @@ jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
